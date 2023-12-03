0c6fb4e0037462785cd51c0e5fc8e4021de58b43
Darshan Panchal <panchaldarshan121@gmail.com>
2023-12-03 13:26:22 +0530

    used dotenv package to secure the variables

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
 const express = require('express')
+require('dotenv').config()
 const app = express()
 
-const port = 3000
+const port = process.env.port
 
 app.get('/', (req,res)=>{
     res.send('Home Page.')
 })
 
-app.get('/github',(req,res)=>{
-    res.send(`darshan's github`)
+app.get('/Hello',(req,res)=>{
+    res.send(`Hello World!!`)
 })
 
 app.listen(port,()=>{
-    console.log(`listening on port${port}`)
+    console.log(`listening on port: ${port}`)
 })
